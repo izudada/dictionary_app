db26c53657c53fc8d55087267352eda3fdcb0101
Anthony <tonyudeagbala@gmail.com>
2022-03-14 15:49:49 +0000

    Create iindex view

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponse
+import requests
 
-# Create your views here.
+
+def index(request):
+    Token = settings.OWL_TOKEN
+    url = " https://owlbot.info/api/v4/dictionary/owl"
+    response = requests.get(url, headers=Token)
+    result = response.json()
+    print(result)
+    return HttpResponse("<h3> Done </h3>")
