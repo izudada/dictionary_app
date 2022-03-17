51e8c3a525076db8d1f4e7fbe484b0a6bdbfee3e
Anthony <tonyudeagbala@gmail.com>
2022-03-17 14:47:39 +0000

    Fix render error

--- dictionary/views.py
+++ dictionary/views.py
@@ -15,26 +15,19 @@ def index(request):
         #   Catching ConnectionError while connecting to owl API
         try:
             url = f"https://owlbot.info/api/v4/dictionary/{word}"
-        except Exception as e:
+        except requests.exceptions.Timeout:
+            messages.success(request, "You have made too many failed attempts")
+            return render(request, "index.html")
+        except requests.exceptions.ConnectionError:
             messages.success(request, "There is an issue with your internet connection")
+            return render(request, "index.html")
+        except requests.exceptions.RequestException:
+            messages.success(request, f"Failed to get the word {word}, please try again")
             return render(request, "index.html")
 
         response = requests.get(url, headers={"Authorization": 'Token ' + Token})
         result = response.json()
 
-        context['word'] = word
-        context['pronunciation'] = result['pronunciation']
-        context["type"] = []
-        context["url"] = []
-        context["definition"] = []
-        context["example"] = []
-
-        for data in result['definitions']:
-            context["url"].append(data['image_url'])
-            context["type"].append(data['type'])
-            context["definition"].append(data['definition'])
-            context["example"].append(data['example'])
-
-        return render(request, "index.html", context)
+        return render(request, "index.html", result)
     else:
         return render(request, "index.html")
