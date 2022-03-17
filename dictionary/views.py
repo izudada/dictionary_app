43a221591cb350f2fb52dbc82c05816ecb6a2df0
Anthony <tonyudeagbala@gmail.com>
2022-03-17 12:47:44 +0000

    Edit index view

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,13 +1,40 @@
 from django.shortcuts import render
 from django.conf import settings
-from django.http import HttpResponse
+from django.contrib import messages
+
 import requests
 
 
 def index(request):
-    Token = settings.OWL_TOKEN
-    url = " https://owlbot.info/api/v4/dictionary/owl"
-    response = requests.get(url, headers=Token)
-    result = response.json()
-    print(result)
-    return HttpResponse("<h3> Done </h3>")
+    
+    if request.method == "POST":
+        word = request.POST.get('word')
+        context = {}
+        Token = settings.OWL_TOKEN
+
+        #   Catching ConnectionError while connecting to owl API
+        try:
+            url = f"https://owlbot.info/api/v4/dictionary/{word}"
+        except Exception as e:
+            messages.success(request, "There is an issue with your internet connection")
+            return render(request, "index.html")
+
+        response = requests.get(url, headers={"Authorization": 'Token ' + Token})
+        result = response.json()
+
+        context['word'] = word
+        context['pronunciation'] = result['pronunciation']
+        context["type"] = []
+        context["url"] = []
+        context["definition"] = []
+        context["example"] = []
+
+        for data in result['definitions']:
+            context["url"].append(data['image_url'])
+            context["type"].append(data['type'])
+            context["definition"].append(data['definition'])
+            context["example"].append(data['example'])
+
+        return render(request, "index.html", context)
+    else:
+        return render(request, "index.html")
