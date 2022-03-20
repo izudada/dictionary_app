a2a3be5a9f90732b4d3819f01ea1066f4c4a15ea
Anthony <tonyudeagbala@gmail.com>
2022-03-20 20:25:11 +0000

    Rewrite request error

--- dictionary/views.py
+++ dictionary/views.py
@@ -11,21 +11,20 @@ def index(request):
         word = request.POST.get('word')
         context = {}
         Token = settings.OWL_TOKEN
+        url = f"https://owlbot.info/api/v4/dictionary/{word}"
 
-        #   Catching ConnectionError while connecting to owl API
         try:
-            url = f"https://owlbot.info/api/v4/dictionary/{word}"
+            response = requests.get(url, headers={"Authorization": 'Token ' + Token})
         except requests.exceptions.Timeout:
             messages.success(request, "You have made too many failed attempts")
             return render(request, "index.html")
         except requests.exceptions.ConnectionError:
-            messages.success(request, "There is an issue with your internet connection")
+            messages.success(request, "There is an issue with your internet connection, please reconnect.")
             return render(request, "index.html")
         except requests.exceptions.RequestException:
             messages.success(request, f"Failed to get the word {word}, please try again")
             return render(request, "index.html")
 
-        response = requests.get(url, headers={"Authorization": 'Token ' + Token})
         result = response.json()
 
         return render(request, "index.html", result)
