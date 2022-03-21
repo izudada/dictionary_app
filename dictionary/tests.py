2bf6a4948dee7f158de9790c253c95beadbcd98a
Anthony <tonyudeagbala@gmail.com>
2022-03-21 22:55:33 +0000

    Create test_index_url_is_resolved

--- dictionary/tests.py
+++ dictionary/tests.py
@@ -1,5 +1,8 @@
 from urllib import response
 from django.test import SimpleTestCase
+from django.urls import resolve, reverse
+
+from .views import index
 
 
 class URRTests(SimpleTestCase):
@@ -13,3 +16,7 @@ class URRTests(SimpleTestCase):
     def test_get_invalid_url(self):
         response = self.client.get('/word')
         self.assertEqual(response.status_code, 404)
+
+    def test_index_url_is_resolved(self):
+        url = reverse('index')
+        self.assertEquals(resolve(url).func, index)
