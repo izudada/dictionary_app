d129c06950c4e8270f485d57177737066ac5d0cb
Anthony <tonyudeagbala@gmail.com>
2022-03-21 19:30:20 +0000

    Create URRTests class

--- dictionary/tests.py
+++ dictionary/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class URRTests(TestCase):
+    def test_get_index_url(self):
+        """
+            Test to successfully get the index page/url
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
